7eb4879973f7f15f024ea3c385562cf53db29d10
Jannis <jannis@pixel-pink.de>
2020-04-14 22:38:14 +0200

    added travis ci

--- .eleventy.js
+++ .eleventy.js
@@ -11,9 +11,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 };
 
-// const CleanCSS = require("clean-css");
-// module.exports = function (eleventyConfig) {
-//   eleventyConfig.addFilter("cssmin", function (code) {
-//     return new CleanCSS({}).minify(code).styles;
-//   });
-// };
+const CleanCSS = require("clean-css");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+};
